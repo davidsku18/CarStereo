c833721a84eaf661b53f3cee7c4a6acf3ee421e7
kurtis <davidsku18@up.edu>
2017-02-09 18:00:03 -0800

    Part 3 and 4 FINAL

--- app/src/main/java/cs371/carstereo/MainActivity.java
+++ app/src/main/java/cs371/carstereo/MainActivity.java
@@ -14,8 +14,10 @@ import android.widget.SeekBar;
 
 public class MainActivity extends Activity {
 
+    int[] amPresets = {550, 600, 650, 700, 750, 800};
+    double[] fmPresets = {90.9, 92.9, 94.9, 96.9, 98.9, 100.9};
     boolean powerOn;
-    boolean am;
+    boolean fm = false;
     double base = 100;
     private double amR;
     private double fmR;
@@ -25,12 +27,10 @@ public class MainActivity extends Activity {
     private Button preset1;
     private Button preset2;
     private Button preset3;
+    private Button preset4;
+    private Button preset5;
+    private Button preset6;
     int AMMIN = 530;
-    int AMMAX = 1700;
-    int FMMIN = 881;
-    int FMMAX = 1079;
-    int amProg=0;
-    int fmProg=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,19 +39,39 @@ public class MainActivity extends Activity {
 
         powerOn=true;
 
-        preset1 = (Button) findViewById(R.id.preset1);
-        preset2 = (Button) findViewById(R.id.preset2);
-        preset3 = (Button) findViewById(R.id.preset3);
         display = (TextView)findViewById(R.id.display);
         power = (ToggleButton) findViewById(R.id.power);
         power.setOnClickListener(new powerButtonListener());
 
 
-
+        seekBarListener tunerListener = new seekBarListener();
         tuner = (SeekBar)findViewById(R.id.tuner);
         tuner.setOnSeekBarChangeListener(new seekBarListener());
 
+
+        preset1 = (Button) findViewById(R.id.preset1);
+        preset2 = (Button) findViewById(R.id.preset2);
+        preset3 = (Button) findViewById(R.id.preset3);
+        preset4 = (Button) findViewById(R.id.preset4);
+        preset5 = (Button) findViewById(R.id.preset5);
+        preset6 = (Button) findViewById(R.id.preset6);
+        presetListener PL = new presetListener();
+
+        preset1.setOnClickListener(PL);
+        preset2.setOnClickListener(PL);
+        preset3.setOnClickListener(PL);
+        preset4.setOnClickListener(PL);
+        preset5.setOnClickListener(PL);
+
+        longButtonListener LB = new longButtonListener();
+        preset1.setOnLongClickListener(LB);
+        preset2.setOnLongClickListener(LB);
+        preset3.setOnLongClickListener(LB);
+        preset4.setOnLongClickListener(LB);
+        preset5.setOnLongClickListener(LB);
+
     }
+
 
     public class powerButtonListener implements View.OnClickListener {
 
@@ -59,51 +79,180 @@ public class MainActivity extends Activity {
 
             if(powerOn){
                 display.setBackgroundColor(0xFFD3D3D3);
-                preset1.setBackgroundColor(0xFFFFFFFF);
-                preset2.setBackgroundColor(0xFFFFFFFF);
-                preset3.setBackgroundColor(0xFFFFFFFF);
                 //powerOn=false;
             }
             else {
-                preset1.setBackgroundColor(0xFF000000);
-                preset2.setBackgroundColor(0xFF000000);
-                preset3.setBackgroundColor(0xFF000000);
                 display.setBackgroundColor(0xFF000000);
                 //powerOn=true;
             }
 
             powerOn = !powerOn;
-
         }
     }
-
     public void amListener(View v){
-        if (am)
+        if (fm)
         {
-            amProg = tuner.getProgress();
-            tuner.setMax((int)(AMMAX-AMMIN));
-            tuner.setProgress(amProg);
+            tuner.setMax(198);
+            tuner.setProgress(99);
+            display.setText("98.0 FM");
+
         }
         else
         {
-            fmProg = tuner.getProgress();
-            tuner.setMax((int)(FMMAX-FMMIN));
-            tuner.setProgress(fmProg);
+            tuner.setMax(1170);
+            tuner.setProgress(585);
+            display.setText("1115 AM");
         }
-        am=!am;
+        fm=!fm;
+    }
+
+    private class longButtonListener implements  View.OnLongClickListener{
+
+        @Override
+        public boolean onLongClick(View view) {
+
+            if(view.getId() == R.id.preset1)
+            {
+                if (!fm) {
+                    fmPresets[0] = ((double)tuner.getProgress() + (double)881)/10;
+                }
+                else
+                {
+                    amPresets[0] = tuner.getProgress()+530;
+                }
+            }
+            else if(view.getId() == R.id.preset2)
+            {
+                if (!fm) {
+                    fmPresets[1] = ((double)tuner.getProgress() + (double)881)/10;
+                }
+                else
+                {
+                    amPresets[1] = tuner.getProgress()+530;
+                }
+            }
+            else if(view.getId() == R.id.preset3)
+            {
+                if (!fm) {
+                    fmPresets[2] = ((double)tuner.getProgress() + (double)881)/10;
+                }
+                else
+                {
+                    amPresets[2] = tuner.getProgress()+530;
+                }
+            }
+            if(view.getId() == R.id.preset4)
+            {
+                if (!fm) {
+                    fmPresets[3] = ((double)tuner.getProgress() + (double)881)/10;
+                }
+                else
+                {
+                    amPresets[3] = tuner.getProgress()+530;
+                }
+            }
+            if(view.getId() == R.id.preset5)
+            {
+                if (!fm) {
+                    fmPresets[4] = ((double)tuner.getProgress() + (double)881)/10;
+                }
+                else
+                {
+                    amPresets[4] = tuner.getProgress()+530;
+                }
+            }
+            if(view.getId() == R.id.preset6)
+            {
+                if (!fm) {
+                    fmPresets[5] = ((double)tuner.getProgress() + (double)881)/10;
+                }
+                else
+                {
+                    amPresets[5] = tuner.getProgress()+530;
+                }
+            }
+            return false;
+        }
+    }
+
+    private class presetListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            if(view.getId() == R.id.preset1)
+            {
+                if (!fm) {
+                    display.setText("" + fmPresets[0] + "FM");
+                }
+                else
+                {
+                    display.setText("" + amPresets[0] + "AM");
+                }
+            }
+            else if(view.getId() == R.id.preset2)
+            {
+                if (!fm) {
+                    display.setText("" + fmPresets[1] + "FM");
+                }
+                else
+                {
+                    display.setText("" + amPresets[1] + "AM");
+                }
+            }
+            else if(view.getId() == R.id.preset3)
+            {
+                if (!fm) {
+                    display.setText("" + fmPresets[2] + "FM");
+                }
+                else
+                {
+                    display.setText("" + amPresets[2] + "AM");
+                }
+            }
+            if(view.getId() == R.id.preset4)
+            {
+                if (!fm) {
+                    display.setText("" + fmPresets[3] + "FM");
+                }
+                else
+                {
+                    display.setText("" + amPresets[3] + "AM");
+                }
+            }
+            if(view.getId() == R.id.preset5)
+            {
+                if (!fm) {
+                    display.setText("" + fmPresets[4] + "FM");
+                }
+                else
+                {
+                    display.setText("" + amPresets[4] + "AM");
+                }
+            }
+            if(view.getId() == R.id.preset6)
+            {
+                if (!fm) {
+                    display.setText("" + fmPresets[5] + "FM");
+                }
+                else
+                {
+                    display.setText("" + amPresets[5] + "AM");
+                }
+            }
+        }
     }
 
     private class seekBarListener implements SeekBar.OnSeekBarChangeListener {
 
-        public void onProgressChanged(SeekBar t, int i, boolean b) {
-            int amVal = AMMIN + amProg;
-            double fmVal = (double) ((FMMIN + fmProg) / 10);
-            if (am) {
-                amProg = tuner.getProgress();
-                display.setText("" + amVal);
-            } else {
-                fmProg = tuner.getProgress();
-                display.setText("" + fmVal);
+        public void onProgressChanged(SeekBar t, int progress, boolean b) {
+            if (fm){
+                display.setText("" + (progress + AMMIN) + "AM");
+            }
+            else {
+                int prevVal, postVal;
+                prevVal = (progress + 881)/10;
+                postVal = ((progress + 881)%10);
+                display.setText("" + prevVal + "." + postVal + "FM");
             }
         }
         public void onStartTrackingTouch(SeekBar seekBar){
